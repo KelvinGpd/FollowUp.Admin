0276daf02e89fbad8c999c22ed112714cca09931
oceane-hays <hays.oceane@icloud.com>
2025-03-09 11:07:36 -0400

    Update Icons.tsx

--- src/components/Icon/Icons.tsx
+++ src/components/Icon/Icons.tsx
@@ -6,6 +6,6 @@ export const ViewPatient = () => (
     <img src={require('./../../img/list.png')} width={40} height={30} />
 )
 
-export const Settings = () => (
-    <img src={require('./../../img/setting.png')} width={40} height={30} />
+export const PrescribeIcon = () => (
+    <img src={require('./../../img/prescribe.png')} width={40} height={30} />
 )
